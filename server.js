5ed8385e6329fcdbce6c5e693967118eb070233d
SeSTA <info@sesta.org>
2021-01-07 13:15:01 +0530

    'g'

--- server.js
+++ server.js
@@ -809,6 +809,6 @@ app.post('/bulk-annual',(req,res)=>{
 
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8080;
 
 app.listen(PORT, () => console.log(`Server started on port ${PORT}`));

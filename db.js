e800ac79f51f593e45d3a96bf3ca08d83856a684
SeSTA <info@sesta.org>
2021-01-07 13:10:37 +0530

    'd'

--- db.js
+++ db.js
@@ -1,30 +1,22 @@
-var mysql = require('mysql');
+// var mysql = require('mysql');
 
- const db = mysql.createConnection({
-   host     : 'localhost',
-   user     : 'root',
-   password : '',
-   database : 'crm', 
-   // port:8889,
+//  const db = mysql.createConnection({
+//    host     : 'localhost',
+//    user     : 'root',
+//    password : '',
+//    database : 'crm', 
+//    // port:8889,
   
- });
+//  });
 
 
-// const db = mysql.createConnection({
-//   host    : 'd1kb8x1fu8rhcnej.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
-//   user     : 'zu0qtl0m23cr5jf0',
-//   password : 'cr0fxd0r70iuh2m7',
-//   database : 'sxs6fatsc4vxudb7', 
-//   port:3306,  
+
+// db.connect((err) => {
+//   if(err){
+//       throw err;
+//   }
+//   console.log('MySql Connected...');
 // });
 
-db.connect((err) => {
-  if(err){
-      throw err;
-  }
-  console.log('MySql Connected...');
-});
+// module.exports = db;
 
-module.exports = db;
-
-// npm run devter
